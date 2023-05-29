3e49e128169343c37aeefdc72b276deb15df4740
Posajpa <131904326+Posajpa@users.noreply.github.com>
2023-05-29 20:15:11 +0200

    model.py

--- model.py
+++ model.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 import torchvision.models as models
 
 
-class VGG(nn.Module):
+class VGG16(nn.Module):
     def __init__(self, config, out):
-        super(VGG, self).__init__()
+        super(VGG16, self).__init__()
         weights = "IMAGENET1K_V1" if config["pretrained"] else None
         self.net = models.vgg16(weights=weights)
+        print(self.net)
         # remove the last FC layer
         num_output_feats = self.net.classifier[-1].in_features  # dim  of the features
         # Initialize a new fully connected layer
@@ -30,12 +31,12 @@ class VGG(nn.Module):
         return x
 
 
-class ResNet(nn.Module):
+class ResNet50(nn.Module):
     def __init__(self, config, out):
-        super(ResNet, self).__init__()
-        # Load a resnet18 from torchvision, either use pretrained weights or not
+        super(ResNet50, self).__init__()
         weights = "IMAGENET1K_V1" if config["pretrained"] else None
-        self.net = models.resnet18(weights=weights)
+        self.net = models.resnet50(weights=weights)
+        print(self.net)
         # remove the last FC layer
         num_output_feats = self.net.fc.in_features   # dim  of the features
         # Initialize a new fully connected layer
@@ -54,5 +55,31 @@ class ResNet(nn.Module):
 
     def get_feature_layer(self, x):
         x = self.base(x)
-        # x = self.fc1(x)
+        return x
+
+
+class EfficientNet(nn.Module):
+    def __init__(self, config, out):
+        super(EfficientNet, self).__init__()
+        weights = "IMAGENET1K_V1" if config["pretrained"] else None
+        self.net = models.efficientnet_b5(weights=weights)
+        print(self.net)
+        # remove the last FC layer
+        num_output_feats = self.net.classifier[-1].in_features  # dim  of the features
+        # Initialize a new fully connected layer
+        self.net.classifier[-1] = torch.nn.Linear(num_output_feats, out)
+
+        for i, param in enumerate(self.net.parameters()):
+            if i < 10:
+                param.requires_grad = False
+        self.base = self.net
+        self.logSoftmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = self.base(x)
+        output = self.logSoftmax(x)
+        return output
+
+    def get_feature_layer(self, x):
+        x = self.base(x)
         return x
